ab48f96a34ebe6d3835eeeb18d3e8c7656338cad
Stanislaviouous <otony.ivanov@yandex.ru>
2021-03-06 17:20:51 +0300

    new

--- src/main/java/api/NewsController.java
+++ src/main/java/api/NewsController.java
@@ -3,20 +3,25 @@ package api;
 
 import model.News;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import service.NewsService;
 
 import java.util.List;
 
 @RestController
+@Controller
 public class NewsController {
 
     @Autowired
     NewsService newsService;
 
-    @GetMapping(value = "/new")
-    public List<News> getAllNews(){
+    @GetMapping("new")
+    public @ResponseBody
+    List<News> getAllNews(){
+
         return newsService.getAllNews();
     }
 }
